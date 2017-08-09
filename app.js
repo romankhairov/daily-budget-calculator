08ffe4662e57df55e299540641c9ca298d5dfb71
Roman Khairov <roman.khairov@gmail.com>
2017-08-09 06:33:39 +0200

    Add dynamic form

--- app.js
+++ app.js
@@ -26,3 +26,18 @@ function findTotalMonthExp(){
     }
     document.getElementById('total_month_exp').value = tot;
 }
+
+// 3) add an other input
+var counter = 1;
+var limit = 3;
+function addInput(divName){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " inputs");
+     }
+     else {
+          var newdiv = document.createElement('div');
+          newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+     }
+}
