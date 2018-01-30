0c4e9135834c51ff9377bf8645359cd8271464cc
Roman Khairov <roman.khairov@gmail.com>
2018-01-30 22:35:40 +0100

    Add plan to the global app controller

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,7 +17,17 @@ var UIController = (function() {
 var controller = (function(budgetCtrl, UICtrl) {
 
   document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log('Button was clicked.')
+
+    // 1. Get the filed input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
   })
 
 })(budgetController, UIController);
