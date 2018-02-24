5f2a752aad9cad80afced35cea61a99b58ba6b6a
Roman <roman.khairov@gmail.com>
2018-02-24 22:10:24 +0100

    add js modules

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
-import MobileMenu from './modules/MobileMenu';
+import BudgetController from './modules/BudgetController';
+import UIController from './modules/UIController';
+import Controller from './modules/Controller';
 
-var mobileMenu = new MobileMenu();
+
+var budgetController = new BudgetController();
+var uIController = new UIController();
+var controller = new Controller();
