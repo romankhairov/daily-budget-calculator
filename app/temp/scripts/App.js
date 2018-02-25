7fe491e72ae5647ad4882e4597b76c5ccf544174
Roman <roman.khairov@gmail.com>
2018-02-25 16:01:05 +0100

    Add steps for Add button functions and key Enter

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -73,29 +73,43 @@
 // BUDGET CONTROLLER-----------------------------------------------------------    BUDGET CONTROLLER    ---------------------------------------------------------------------
 var budgetController = function () {
 
-  // Some code
+    // Some code
 
 }();
 
 // UI CONTROLLER---------------------------------------------------------------    UI CONTROLLER    ---------------------------------------------------------------------
 var uiController = function () {
 
-  // Some code
+    // Some code
 
 }();
 
 // GLOBAL APP CONTROLLER-------------------------------------------------------    GLOBAL APP CONTROLLER    ---------------------------------------------------------------------
 var controller = function (budgetController, uiController) {
 
-  document.querySelector('.add__btn').addEventListener('click', function () {
+    var ctrlAddItem = function ctrlAddItem() {
 
-    // 1. Get the filed input data
-    // 2. Add the item to the budget controller
-    // 3. Add the item to the UI
-    // 4. Calculate the budget
-    // 5. Display the budget on the UI
+        // 1. Get the filed input data
 
-  });
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('Test of ctrlAddItem');
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (e) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 }(budgetController, uiController);
 
 /***/ })
